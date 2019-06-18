fbbc1be28899f9b59a39014e38564755476abffb
Dan Kroszner <dankroz@gmail.com>
2019-06-18 14:00:31 -0400

    setup

---
+++ my-app/src/components/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import NavBar from "./navbar";
+import Jumbotron from "./jumbotron";
+
+class Index extends Component {
+    state = {
+        score: 0,
+        highScore: 0
+    };
+};
